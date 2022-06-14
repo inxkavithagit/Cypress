cb3e5c9d041064138af59cf721f1b2e392a9be4d
kavitha <kavitha.selvaraj@inxsoftware.com>
2022-06-14 15:25:36 +0530

    init

---
+++ First test 1.js
@@ -0,0 +1,11 @@
+describe ('Test suite 1' , function()
+{
+    it ('verify first test case', function()
+    {
+        cy.visit('www.google.com')
+        cy.title().should('eq', 'Google')
+        cy.get('.SDkEP')
+        cy.get('.gLFyf')
+        cy.get('.gLFyf')
+    })
+})
